d5fc9cf7c9c4c134433397e69635b69e1d58b10d
Lachgar Youssef <lachgar.youssef0@gmail.com>
2024-06-03 22:09:22 +0100

    adding cockpit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
@@ -12,7 +13,8 @@ import { ServerElementComponent } from './server-element/server-element.componen
     ServerElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
   templateUrl: './cockpit.component.html',
   styleUrls: ['./cockpit.component.css']
 })
-export class CockpitComponent {
+export class CockpitComponent implements OnInit {
 
+  serverElements: Array<Object> = [];
+  newServerName = "";
+  newServerContent = "";
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onAddServer() {
+    this.serverElements.push({
+      tyep: "server",
+      name: this.newServerName,
+      content: this.newServerContent
+    })
+  }  
+  onAddBlueprint() {
+    this.serverElements.push({
+      tyep: "blueprint",
+      name: this.newServerName,
+      content: this.newServerContent
+    })
+  }
 }
